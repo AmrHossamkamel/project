7f3732ea382673343b1eac0f152ebf03c001e0a1
solidpointai <solidpointai@gmail.com>
2025-11-13 04:42:08 +0200

    main project

---
+++ resources/views/admin-views/business-settings/business-setup-inline-menu.blade.php
@@ -0,0 +1,51 @@
+<div class="position-relative nav--tab-wrapper mb-4">
+    <ul class="nav nav-pills nav--tab me-5" id="pills-tab" role="tablist">
+        <li class="nav-item">
+            <a class="nav-link {{ Request::is('admin/business-settings/web-config') ? 'active' : '' }}"
+                href="{{ route('admin.business-settings.web-config.index') }}">
+                {{ translate('General') }}
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link {{ Request::is('admin/business-settings/website-setup') ? 'active' : '' }}"
+            href="{{ route('admin.business-settings.website-setup') }}">
+                {{ translate('Website_Setup') }}
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link {{ Request::is('admin/business-settings/product-settings') ? 'active' : '' }}"
+                href="{{ route('admin.business-settings.product-settings.index') }}">
+                {{ translate('Products') }}
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link {{ Request::is('admin/business-settings/customer-settings') ? 'active' : '' }}"
+                href="{{ route('admin.business-settings.customer-settings') }}">
+                {{ translate('Customer') }}
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link {{ Request::is('admin/business-settings/order-settings/index') ? 'active' : '' }}"
+                href="{{ route('admin.business-settings.order-settings.index') }}">
+                {{ translate('Orders') }}
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link {{ Request::is('admin/business-settings/shipping-method/index') ? 'active' : '' }}"
+                href="{{ route('admin.business-settings.shipping-method.index') }}">
+                {{ translate('Shipping_Method') }}
+            </a>
+        </li>
+    </ul>
+    <div class="nav--tab__prev">
+        <button class="btn btn-circle border-0 bg-white text-primary">
+            <i class="fi fi-sr-angle-left"></i>
+        </button>
+    </div>
+    <div class="nav--tab__next">
+        <button class="btn btn-circle border-0 bg-white text-primary">
+            <i class="fi fi-sr-angle-right"></i>
+        </button>
+    </div>
+
+</div>
---
+++ resources/views/admin-views/system-setup/system-settings-inline-menu.blade.php
@@ -0,0 +1,30 @@
+<div class="position-relative nav--tab-wrapper mb-4">
+    <ul class="nav nav-pills nav--tab" id="pills-tab" role="tablist">
+
+        <li class="nav-item">
+            <a class="nav-link {{ Request::is('admin/system-setup/environment-setup') ? 'active' : '' }}"
+               href="{{ route('admin.system-setup.environment-setup') }}">
+                {{ translate('Environment_Settings') }}
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link {{ Request::is('admin/system-setup/language') ? 'active' : '' }}"
+               href="{{ route('admin.system-setup.language.index') }}">
+                {{ translate('Language') }}
+            </a>
+        </li>
+
+    </ul>
+    <div class="nav--tab__prev">
+        <button class="btn btn-circle border-0 bg-white text-primary">
+            <i class="fi fi-sr-angle-left"></i>
+        </button>
+    </div>
+    <div class="nav--tab__next">
+        <button class="btn btn-circle border-0 bg-white text-primary">
+            <i class="fi fi-sr-angle-right"></i>
+        </button>
+    </div>
+
+</div>
